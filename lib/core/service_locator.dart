c43117187a870d0195e72616890c793ad417922c
sheha <shehabehab1029@gmail.com>
2024-06-20 21:11:31 +0300

    add new package and add sl

---
+++ lib/core/service_locator.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+import 'package:slash_app/features/home_page/data/repositories/products_repository.dart';
+import 'package:slash_app/features/home_page/data/sources/product_local_data_source.dart';
+import 'package:slash_app/features/home_page/domain/repositories/product_repository_interface.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initServicesLocator() async {
+  //#region Repos
+  sl.registerLazySingleton<BaseProductsRepository>(
+      () => ProductsRepository(sl.get()));
+  //#endregion
+
+  //#region Data Sources
+  sl.registerLazySingleton<BaseProductLocalDataSource>(
+      () => ProductLocalDataSource());
+  //#endregion
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:slash_app/core/service_locator.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initServicesLocator();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
