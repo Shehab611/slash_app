c069b8a52e2f3983d51db07fa0db67f4c8a35bd3
sheha <shehabehab1029@gmail.com>
2024-06-20 22:19:08 +0300

    add some utils

--- lib/core/utils/design_utils/app_sizes.dart
+++ lib/core/utils/design_utils/app_sizes.dart
@@ -1,7 +1,6 @@
 part of 'app_theme.dart';
 
 abstract final class AppSizes {
-
   /// font size 14
   static const double fontSizeDefault = 14.0;
 
@@ -11,4 +10,12 @@ abstract final class AppSizes {
   /// font size 20
   static const double fontSizeVeryLarge = 22.0;
 
+  /// padding size 8
+  static const double defaultPaddingSize = 8.0;
+
+  /// padding size 12
+  static const double largePaddingSize = 12.0;
+
+  /// padding size 18
+  static const double veryLargePaddingSize = 18.0;
 }
