2e4833bc198f54fa8211077d1f8c2155fab49cf5
sheha <shehabehab1029@gmail.com>
2024-06-20 20:30:17 +0300

    make some edits

--- lib/core/responses/data_response.dart
+++ lib/core/responses/data_response.dart
@@ -2,13 +2,13 @@ import 'package:equatable/equatable.dart';
 
 final class Response<T> extends Equatable {
   final String? error;
-  final List<T>? data;
+  final T? data;
 
   const Response({this.error, this.data});
 
   const Response.withError(String this.error) : data = null;
 
-  const Response.withSuccess(List<T> this.data) : error = null;
+  const Response.withSuccess(T this.data) : error = null;
 
   @override
   List<Object?> get props => [error, data];
--- lib/features/home_page/domain/repositories/product_repository_interface.dart
+++ lib/features/home_page/domain/repositories/product_repository_interface.dart
@@ -2,9 +2,9 @@ import 'package:slash_app/core/responses/data_response.dart';
 import 'package:slash_app/features/home_page/domain/entities/product.dart';
 
 abstract interface class ProductRepository {
-  Future<Response<Product>> getBestSelling();
+  Future<Response<List<Product>>> getBestSelling();
 
-  Future<Response<Product>> getNewArrival();
+  Future<Response<List<Product>>> getNewArrival();
 
-  Future<Response<Product>> getRecommended();
+  Future<Response<List<Product>>> getRecommended();
 }
