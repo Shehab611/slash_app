6c36dd828699ad18f9ab1410a7541cb61645f04a
sheha <shehabehab1029@gmail.com>
2024-06-20 20:34:46 +0300

    edit in entity

--- lib/features/home_page/domain/entities/product.dart
+++ lib/features/home_page/domain/entities/product.dart
@@ -12,23 +12,6 @@ final class Product extends Equatable {
       required this.price,
       required this.image});
 
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-        id: json['id'],
-        name: json['name'],
-        price: json['price'],
-        image: json['image']);
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'price': price,
-      'image': image,
-    };
-  }
-
   @override
   List<Object> get props => [id, name, price, image];
 }
