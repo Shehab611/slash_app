6b406c4e313c1b257029f10fec0f58f1f0038801
sheha <shehabehab1029@gmail.com>
2024-06-20 23:10:30 +0300

    edit on font

--- lib/core/utils/design_utils/app_text_styles.dart
+++ lib/core/utils/design_utils/app_text_styles.dart
@@ -3,17 +3,17 @@ part of 'app_theme.dart';
 abstract final class AppTextStyles {
   //#region App Default Text Styles
 
-  static TextStyle defaultTextStyle = TextStyle(
+  static TextStyle defaultTextStyle = GoogleFonts.urbanist(
       color: AppConstants.defaultColor,
       fontSize: AppSizes.fontSizeDefault,
       fontWeight: FontWeight.w500);
 
-  static TextStyle titleTextStyle = TextStyle(
+  static TextStyle titleTextStyle = GoogleFonts.urbanist(
       color: AppConstants.defaultColor,
       fontSize: AppSizes.fontSizeLarge,
       fontWeight: FontWeight.w600);
 
-  static TextStyle itemTextStyle = TextStyle(
+  static TextStyle itemTextStyle = GoogleFonts.urbanist(
       color: AppConstants.defaultColor,
       fontSize: AppSizes.fontSizeVeryLarge,
       fontWeight: FontWeight.w600);
@@ -21,7 +21,7 @@ abstract final class AppTextStyles {
   //#endregion
 
   //#region App Bar Text Style
-  static TextStyle appBarTitleTextStyle = TextStyle(
+  static TextStyle appBarTitleTextStyle = GoogleFonts.urbanist(
       color: AppConstants.defaultColor,
       fontSize: AppSizes.fontSizeLarge,
       fontWeight: FontWeight.w700);
@@ -29,12 +29,12 @@ abstract final class AppTextStyles {
 //#endregion
 
 //#region Bottom Navigation Bar Text Styles
-  static TextStyle unSelectedTextStyle = TextStyle(
+  static TextStyle unSelectedTextStyle = GoogleFonts.urbanist(
       color: AppConstants.defaultColor,
       fontSize: AppSizes.fontSizeDefault,
       fontWeight: FontWeight.w400);
 
-  static TextStyle selectedTextStyle = TextStyle(
+  static TextStyle selectedTextStyle = GoogleFonts.urbanist(
       color: AppConstants.defaultColor,
       fontSize: AppSizes.fontSizeDefault,
       fontWeight: FontWeight.w700);
--- lib/core/utils/design_utils/app_theme.dart
+++ lib/core/utils/design_utils/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:slash_app/core/utils/app_constants/app_constants.dart';
 
 part 'app_sizes.dart';
